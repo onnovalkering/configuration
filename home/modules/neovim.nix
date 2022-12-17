bd44396991d053c846563103a704ac9e500283e0
Onno Valkering <onnovalkering@users.noreply.github.com>
2022-12-17 08:57:42 +0100

    Reset neovim config

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -13,19 +13,6 @@
       '';
 
       plugins = with pkgs.vimPlugins; [
-        completion-nvim
-        lspsaga-nvim
-        lsp-status-nvim
-        lualine-lsp-progress
-        lualine-nvim
-        nvim-lspconfig
-        nvim-tree-lua
-        nvim-treesitter
-        nvim-web-devicons
-        packer-nvim
-        rust-tools-nvim
-        telescope-fzy-native-nvim
-        telescope-nvim
       ];
     };
   };

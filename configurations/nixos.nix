dee1d40f96f1200abe6a37962e0865bef57c966b
Onno Valkering <onnovalkering@users.noreply.github.com>
2024-10-20 18:21:19 +0200

    Autoconnect Tailscale

--- configurations/nixos.nix
+++ configurations/nixos.nix
@@ -69,4 +69,27 @@
   services.tailscale = {
     openFirewall = true;
   };
+
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # Make sure tailscale is running before trying to connect to tailscale.
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig.Type = "oneshot";
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      ${tailscale}/bin/tailscale up
+    '';
+  };
 }

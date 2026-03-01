c75c90d7bc188d16a6b81bde379fa52ea1e8defb
Onno Valkering <onnovalkering@users.noreply.github.com>
2026-03-01 18:55:29 +0100

    chore(flake): create dev.nix flake part

--- flake.nix
+++ flake.nix
@@ -51,47 +51,10 @@
       ];
 
       imports = [
+        ./dev.nix
         ./hosts/elitebook.nix
         ./hosts/macbook-pro.nix
         ./hosts/server-vesta.nix
       ];
-
-      perSystem =
-        {
-          config,
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          checks.pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              deadnix.enable = true;
-              luacheck.enable = true;
-              nixfmt-rfc-style.enable = true;
-              prettier.enable = true;
-              shellcheck.enable = true;
-              shfmt = {
-                enable = true;
-                settings.indent = 4;
-              };
-              statix.enable = true;
-              stylua.enable = true;
-              typos.enable = true;
-            };
-          };
-
-          devShells.default = pkgs.mkShell {
-            inherit (config.checks.pre-commit-check) shellHook;
-
-            buildInputs = with pkgs; [
-              deadnix
-              nil
-              nixfmt-rfc-style
-              statix
-            ];
-          };
-        };
     };
 }
